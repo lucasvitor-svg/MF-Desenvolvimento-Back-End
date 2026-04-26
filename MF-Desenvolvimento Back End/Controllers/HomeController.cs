fe7bd2c5e18ac80577efd45bc5903ad8adff8f58
Lucas Vitor <lucasvitor9814@gmail.com>
2026-04-26 19:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ MF-Desenvolvimento Back End/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using MF_Desenvolvimento_Back_End.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MF_Desenvolvimento_Back_End.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MF-Desenvolvimento Back End/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MF_Desenvolvimento_Back_End.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
